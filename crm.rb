25bef6dd0f3d4c9ddebe077fcc7e469eca359c6a
NATHAN <nathan.ho28@gmail.com>
2014-10-24 21:33:52 -0400

    working modify_contact function

--- crm.rb
+++ crm.rb
@@ -1,14 +1,16 @@
-require_relative './contact.rb' #logically separating by class and by file
+#as a user i am presented with a promt to 'add', 'modify', 'diplay all', 
+#'display contact', 'diplay attribute', 'delete' and 'exit'.
+require_relative './contact.rb'
 require_relative './rolodex.rb'
 
 class CRM
 	attr_accessor :name
-	def initialize(name) #.new means initialize and is looking for this method first.
+	def initialize(name) #initialize means new 
 		@name = name
 		@rolodex = Rolodex.new
 	end
-	def print_main_menu
 
+	def print_main_menu
 		puts "[1] Add a contact"
 		puts "[2] Modify a contact"
 		puts "[3] Display all contacts"
@@ -16,71 +18,73 @@ class CRM
 		puts "[5] Display an attribute"
 		puts "[6] Delete a contact"
 		puts "[7] Exit"
-		puts "Enter a number: "
+		puts "Enter a number:"
 	end
 
-
 	def main_menu
-		puts "Welcome to #{@name}"
-		while true
+		puts "Welcome to #{@name}!"
+
+			while true
 			print_main_menu
 			input = gets.chomp.to_i
-			choose_option(input) #passing off the input into another method
 			return if input == 7
-		end
-		# puts "input at main_menu: #{input}" #1
+		choose_option(input)
+			end
 	end
 
 	def choose_option(option)
-		case option #case can be on multiple lines like if statement or one line.
+		case option
 		when 1 then add_contact
 		when 2 then modify_contact
 		when 3 then display_all_contacts
 		when 4 then display_one_contact
 		when 5 then display_attribute
 		when 6 then delete_contact
-		when 7
+		when 7 
 			puts "Goodbye!"
-			return
-		else "Invalid input. Try again!"
+		else
+			puts "please use one of the given numbers"
 		end
-			
-		# puts "input at choose_option: #{option}" #1
-		# option = 12
-		# puts "input at choose_option (after change) #{option}" #12
 	end
 
 	def add_contact
-		print "First Name: "
+		print "First name: "
 		first_name = gets.chomp
-		print "Last Name: "
+		print "Last name: "
 		last_name = gets.chomp
 		print "Email: "
 		email = gets.chomp
 		print "Note: "
 		note = gets.chomp
-		
+
 		contact = Contact.new(first_name, last_name, email, note)
 		@rolodex.add_contact(contact)
 	end
 
 	def display_all_contacts
 		@rolodex.contacts.each do |contact|
-			puts "#{contact.first_name} #{contact.last_name} <#{contact.email}> (#{contact.id}"
+			puts "#{contact.first_name}, #{contact.last_name}, #{contact.email} (#{contact.id})"
 		end
 	end
 
 	def delete_contact
-		puts "Please enter the contact id you would like to delete (ie. 1xxx): "
+		puts "Type the contact id number you want to delete I.E: 1xxx: "
 		id_delete = gets.chomp.to_i
 		@rolodex.delete_contact(id_delete)
-		puts "ID #{id_delete} has been deleted!"
+		puts "Contact #{id_delete}was deleted"
 	end
+
+	def modify_contact
+		puts "Type the contact id number you want to modify I.E. 1xxx: "
+		id = gets.chomp.to_i
+		@rolodex.find(id)
+		puts "What do you want to change about the contact?"
+		puts "I.E.: first name, last name, email or note."
+		change = gets.chomp.downcase
+		@rolodex.modify_contact(change)
+	end
+
 end
 
-
-crm = CRM.new("Bitmaker Labs CRM")
+crm = CRM.new("Bitmaker labs CRM")
 crm.main_menu
-
-
-
--- rolodex.rb
+++ rolodex.rb
@@ -1,9 +1,15 @@
+require "pry"
+
 class Rolodex
-	attr_reader :contacts
-	@@index = 1000 #if this was to be a class variable, the identifier would be unique on a global level
-	
+attr_reader :contacts
+@@index = 1000 #if this was to be a class variable, the identifier would be unique on a global level
+
 	def initialize
-		@contacts = []
+		@contacts = [] #empty arrary to hold entire contact list.
+	end
+
+	def find(id) #search method to be reused in several other methods.
+		@found = @contacts.select{|contact| contact.id == id }.first
 	end
 
 	def add_contact(contact)
@@ -13,6 +19,29 @@ class Rolodex
 	end
 
 	def delete_contact(id_delete)
-		@contacts.delete_if {|contact| contact.id = id_delete }
+		@contacts.delete_if{|contact| contact.id == id_delete}
 	end
+
+	def modify_contact(change)
+		case change
+		when "first name"
+			puts "New first name: "
+			#@found represents the now isolated objected within the array.
+			@found.first_name = gets.chomp 
+		when "last name"
+			puts "New last name: "
+			@found.last_name = gets.chomp
+		when "email"
+			puts "New email: "
+			@found.email = gets.chomp
+		when "note"
+			puts "New notes: "
+			@found.note = gets.chomp
+		else
+			puts "Error use a specified command"
+		end
+	end
+
+		
 end
+
